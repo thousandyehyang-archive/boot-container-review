b08a7f3ddcafebbc006c292e11ed0aedbea49ef3
1000hyehyang <ducogus12@gmail.com>
2025-05-22 10:36:24 +0900

    feat: 기본 api

---
+++ src/main/java/org/example/bootcontainerreview/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package org.example.bootcontainerreview.repository;
+
+import org.example.bootcontainerreview.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+}
